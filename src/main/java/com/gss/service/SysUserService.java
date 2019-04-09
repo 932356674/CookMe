7d8aca837991f0999ac0add2eb4f68266b68bda5
932356674 <932356674@qq.com>
2019-04-09 22:10:21 +0800

    Test

--- src/main/java/com/gss/service/SysUserService.java
+++ src/main/java/com/gss/service/SysUserService.java
@@ -5,7 +5,7 @@ import com.gss.utils.R;
 public interface SysUserService {
 
 
-
+    public R selectMobile();
 
 
 }
---
+++ src/main/java/com/gss/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.gss.service.impl;
+
+import com.gss.service.SysUserService;
+import com.gss.utils.R;
+
+public class SysUserServiceImpl implements SysUserService {
+    @Override
+    public R selectMobile() {
+        return null;
+    }
+}
