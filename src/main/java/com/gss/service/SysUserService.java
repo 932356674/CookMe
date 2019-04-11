e00988764e002c903ba395fa57feeb85c7ab9c42
aiyouqiang <2425962185@qq.com>
2019-04-11 20:26:54 +0800

    cgq

--- src/main/java/com/gss/service/SysUserService.java
+++ src/main/java/com/gss/service/SysUserService.java
@@ -4,9 +4,10 @@ import com.gss.entity.User;
 import com.gss.utils.R;
 
 public interface SysUserService {
+    //个人主页
     public R selectMyHome(Integer usId);
-
+    //完善个人信息
     public R changeInfoById(User user);
-
+    //修改密码
     public R updatePassword(Integer usId,String oldPassword,String newPasswords);
 }
--- src/main/java/com/gss/test/TestMD5.java
+++ src/main/java/com/gss/test/TestMD5.java
@@ -8,6 +8,7 @@ public class TestMD5 {
         Md5Hash md5Hash = new Md5Hash("admin","18888888888",1024);
         System.out.println(md5Hash.toString());
 
+         md5Hash = new Md5Hash("string","33333333333",1024);
          md5Hash = new Md5Hash("string","22222222222",1024);
         System.out.println(md5Hash.toString());
     }
