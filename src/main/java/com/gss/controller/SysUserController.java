ee1c5c33ed1dfb8a17e4b5548c8c7a10ac96a837
zhao <179312577@qq.com>
2019-04-11 19:30:25 +0800

    zjw5

--- src/main/java/com/gss/controller/SysUserController.java
+++ src/main/java/com/gss/controller/SysUserController.java
@@ -14,6 +14,7 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/sys")
