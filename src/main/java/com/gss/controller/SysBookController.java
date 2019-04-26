48dd1fded4d325c23c3dfea07838ddd84481333e
caizhenhua <dloucaizhenhua@163.com>
2019-04-26 17:19:56 +0800

    Merge branch 'master' of C:\Users\caimeiwei\IdeaProjects\CookMe with conflicts.

--- src/main/java/com/gss/controller/SysBookController.java
+++ src/main/java/com/gss/controller/SysBookController.java
@@ -64,14 +64,14 @@ public class SysBookController {
     }
     //根据菜谱类型获得菜谱简略信息
     @ApiOperation(value = "根据菜谱类型查询",notes = "根据菜谱类型获得菜谱简略信息")
-    @RequestMapping(value = "/book/selectByType",method = RequestMethod.GET)
-    public ResultData selectByType(@RequestParam("typeId") Integer typeId , @RequestBody Pager pager){
+    @RequestMapping(value = "/book/selectByType/{typeId}",method = RequestMethod.POST)
+    public ResultData selectByType(@PathVariable Integer typeId ,@RequestBody Pager pager){
         return sysBookService.selectByType(typeId,pager);
     }
 
     @ApiOperation(value = "查询首页时间菜谱",notes = "根据菜谱类型获得菜谱简略信息")
     @RequestMapping(value = "/book/selectByTimeType",method = RequestMethod.GET)
-    public R selectByTimeType(@RequestParam("typeId") Integer typeId){
+    public R selectByTimeType(int typeId){
         return sysBookService.selectByTimeType(typeId);
     }
     @ApiOperation(value = "查询首页推荐菜谱",notes = "获得菜谱简略信息")
